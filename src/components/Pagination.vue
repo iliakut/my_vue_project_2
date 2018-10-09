eb1b81f39f396793b8a81ba7df04b2c2a8b425b0
Ilia <ilyakut@gmail.com>
2018-10-09 16:29:59 +0300

    numbers of users on page

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,5 +1,10 @@
 <template>
-  <rows-per-page/>
+  <div>
+    <p>Количество пользователей на странице {{ usersPerPage }}</p>
+    <rows-per-page
+      @changeRaws="rows => usersPerPage = rows"
+    />
+  </div>
 </template>
 
 <script>
@@ -7,6 +12,9 @@ export default {
   name: 'Pagination',
   components: {
     'rows-per-page': () => import('@/components/Pagination/RowsPerPage')
-  }
+  },
+  data: () => ({
+    usersPerPage: 0
+  })
 }
 </script>
--- src/components/Pagination/RowsPerPage.vue
+++ src/components/Pagination/RowsPerPage.vue
@@ -1,11 +1,23 @@
 <template>
-  <select>
-    Количество пользователей на странице
-  </select>
+  <div>
+    <select :value="value" @input="changed($event.target.value)">
+      <option v-for="rows in rowsOnPage" :key="rows" :value="rows">
+        {{ rows }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'RowsPerPage'
+  name: 'RowsPerPage',
+  data: () => ({
+    rowsOnPage: [2, 5, 10, 100]
+  }),
+  methods: {
+    changed(value) {
+      this.$emit('changeRaws', value)
+    }
+  }
 }
 </script>

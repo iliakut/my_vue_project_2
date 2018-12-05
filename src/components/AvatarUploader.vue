d0474d500bd4ba0caf18fa5781b14b56b91d2c01
Ilia <ilyakut@gmail.com>
2018-12-05 16:13:59 +0300

    avatar

---
+++ src/components/AvatarUploader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <div class="input-group-prepend">
+        <span class="input-group-text">Аватарка</span>
+      </div>
+      <input
+        v-model="picture"
+        type="text"
+        class="form-control mr-sm-2"
+      >
+    </div>
+    <div class="input-group mb-3">
+      <img :src="picture">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvatarUploader',
+  model: {
+    //prop: 'picture'
+  },
+  props: {
+    picture: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
